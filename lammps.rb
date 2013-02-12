8e92a77e37581813b629ab37cce0e0ad1ff7de83
Derek Thomas <derekathomas@gmail.com>
2013-02-12 23:21:23 +0900

    Refactor lammps formula and fix OpenMP implementation
    This includes the following changes
    
    1. Single, unified library build function: more maintainable and easier
       to read
    2. Support for fortran compiler other than gfortran (use FC)
    3. Better enforcement of clang
    4. Fix OpenMP installation
      * depends on 'homebrew/dupes/gcc' because some functions in gcc42 omp
        fail to build
    5. Patch for a bug introduced in the 2013.02.11 version of LAMMPS
       user-omp
    6. Roll back incorrect version-number explanation. It turns out that it
       is 'days' after all
    7. Add voro++ dependency

--- lammps.rb
+++ lammps.rb
@@ -2,24 +2,25 @@ require 'formula'
 
 class Lammps < Formula
   homepage 'http://lammps.sandia.gov'
-  url 'http://lammps.sandia.gov/tars/lammps-12Jan13.tar.gz'
-  sha1 'da31b931dc38d00fef2e5d61b924154c5d538240'
+  url 'http://lammps.sandia.gov/tars/lammps-12Feb13.tar.gz'
+  sha1 'e4c1cc179e8159e7bd2dd958d3f5c8909a315af8'
   # lammps releases are named after their release date. We transform it to
-  # YYYY.MM.RR (year.month.revision) so that we get a comparable version numbering (for brew outdated)
-  version '2013.01.12'
+  # YYYY.MM.DD (year.month.day) so that we get a comparable version numbering (for brew outdated)
+  version '2013.02.12'
   head 'http://git.icms.temple.edu/lammps-ro.git'
 
   # user-submitted packages not considered "standard"
+  # 'user-omp' must be last
   USER_PACKAGES= %W[
     user-misc
     user-awpmd
     user-cg-cmm
     user-colvars
     user-eff
-    user-omp
     user-molfile
     user-reaxc
     user-sph
+    user-omp
   ]
 
   # could not get gpu or user-cuda to install (hardware problem?)
@@ -44,40 +45,48 @@ class Lammps < Formula
 
   depends_on 'fftw'
   depends_on 'jpeg'
+  depends_on 'voro++'
   depends_on MPIDependency.new(:cxx, :f90) if build.include? "with-mpi"
+  depends_on 'homebrew/dupes/gcc' if build.include? "enable-user-omp"
 
-  def build_f90_lib(lmp_lib)
-    # we currently assume gfortran is our fortran library
+  def build_lib(comp, lmp_lib, opts={})
+    non_std_repl = opts[:non_std_repl]  # a non-standard compiler name to replace
+    var_add = opts[:var_add] || ""      # prepended to makefile variable names
+
     cd "lib/"+lmp_lib do
-      make_file = "Makefile.gfortran"
-      if build.include? "with-mpi"
-        inreplace make_file do |s|
-          s.change_make_var! "F90", ENV["MPIFC"]
-        end
+      if comp == "FC"
+        make_file = "Makefile.gfortran" # make file
+        repl = "F90"                    # replace compiler
+      elsif comp == "CXX"
+        make_file = "Makefile.g++"      # make file
+        repl = "CC"                     # replace compiler
+        repl = non_std_repl if non_std_repl
+      elsif comp == "MPICXX"
+        make_file = "Makefile.openmpi"  # make file
+        repl = "CC"                     # replace compiler
+        comp = "CXX"                    # Reduntant since this returns to MPICXX
       end
+
+      compiler = ENV[comp]
+      if build.include? "with-mpi"
+        compiler = ENV["MPI" + comp]
+      end
+
+      # force compiler
+      inreplace make_file do |s|
+        s.change_make_var! repl, compiler
+      end
+
       system "make", "-f", make_file
 
-      ENV.append 'LDFLAGS', "-lgfortran -L#{Formula.factory('gfortran').opt_prefix}/gfortran/lib"
-
-      # empty it to reduce chance of conflicts
-      inreplace "Makefile.lammps" do |s|
-        s.change_make_var! lmp_lib+"_SYSINC", ""
-        s.change_make_var! lmp_lib+"_SYSLIB", "-lgfortran"
-        s.change_make_var! lmp_lib+"_SYSPATH", ""
-      end
-    end
-  end
-
-  def build_cxx_lib(lmp_lib)
-    cd "lib/"+lmp_lib do
-      make_file = "Makefile.g++"
-      if build.include? "with-mpi"
-        make_file = "Makefile.openmpi" if File.exists? "Makefile.openmpi"
-        inreplace make_file do |s|
-          s.change_make_var! "CC" , ENV["MPICXX"]
+      if File.exists? "Makefile.lammps"
+        # empty it to reduce chance of conflicts
+        inreplace "Makefile.lammps" do |s|
+          s.change_make_var! var_add+lmp_lib+"_SYSINC", ""
+          s.change_make_var! var_add+lmp_lib+"_SYSLIB", ""
+          s.change_make_var! var_add+lmp_lib+"_SYSPATH", ""
         end
       end
-      system "make", "-f", make_file
     end
   end
 
@@ -85,22 +94,36 @@ class Lammps < Formula
     ENV.j1      # not parallel safe (some packages have race conditions :meam:)
     ENV.fortran # we need fortran for many packages, so just bring it along
 
-    build_f90_lib "reax"
-    build_f90_lib "meam"
-    build_cxx_lib "poems"
-    build_cxx_lib "awpmd" if build.include? "enable-user-awpmd" and build.include? "with-mpi"
-    if build.include? "enable-user-colvars"
-      # the makefile is craeted by a user and is not of standard format
-      cd "lib/colvars" do
-        make_file = "Makefile.g++"
-        if build.include? "with-mpi"
-          inreplace make_file do |s|
-            s.change_make_var! "CXX" , ENV["MPICXX"]
-          end
-        end
-        system "make", "-f", make_file
-      end
+    # make sure to optimize the installation
+    ENV.append "CFLAGS","-O"
+    ENV.append "LDFLAGS","-O"
+
+    if build.include? "enable-user-omp"
+      # OpenMP requires the latest gcc
+      ENV["CXX"] = Formula.factory('homebrew/dupes/gcc').opt_prefix/"bin/g++"
+
+      # The following should be part of MPIDependency mxcl/homebrew#17370
+      ENV["OMPI_MPICXX"] = ENV["CXX"]              # correct the openmpi wrapped compiler
+      # mpich2 needs this, but it would throw an error without mpich2. Therefore, I leave it out
+      # ENV.append "CFLAGS", "-CC=#{ENV['CXX']}"     # mpich2   wrapped compiler
+
+      # Build with OpenMP
+      ENV.append "CFLAGS",  "-fopenmp"
+      ENV.append "LDFLAGS", "-L#{Formula.factory('homebrew/dupes/gcc').opt_prefix}/gcc/lib -lgomp"
     end
+
+    # build package libraries
+    build_lib "FC",    "reax"
+    build_lib "FC",    "meam"
+    build_lib "CXX",   "poems"
+    build_lib "CXX",   "colvars", :non_std_repl => "CXX"  if build.include? "enable-user-colvars"
+    if build.include? "enable-user-awpmd" and build.include? "with-mpi"
+      build_lib "MPICXX","awpmd",   :var_add => "user-"
+      ENV.append 'LDFLAGS', "-lblas -llapack"
+    end
+
+    # we currently assume gfortran is our fortran library
+    ENV.append 'LDFLAGS', "-L#{Formula.factory('gfortran').opt_prefix}/gfortran/lib -lgfortran"
 
     # build the lammps program and library
     cd "src" do
@@ -115,9 +138,12 @@ class Lammps < Formula
           s.change_make_var! "LINK"   , ENV["MPICXX"]
 
           #-DOMPI_SKIP_MPICXX is to speed up c++ compilation
-          s.change_make_var! "MPI_INC"  , "-DOMPI_SKIP_MPICXX -I#{HOMEBREW_PREFIX}/include"
-          s.change_make_var! "MPI_PATH" , "-L#{HOMEBREW_PREFIX}/lib"
+          s.change_make_var! "MPI_INC"  , "-DOMPI_SKIP_MPICXX"
+          s.change_make_var! "MPI_PATH" , ""
           s.change_make_var! "MPI_LIB"  , ""
+        else
+          s.change_make_var! "CC"   , ENV["CXX"]
+          s.change_make_var! "LINK" , ENV["CXX"]
         end
 
         # installing with FFTW and JPEG
@@ -129,9 +155,12 @@ class Lammps < Formula
         s.change_make_var! "JPG_PATH" , "-L#{Formula.factory('jpeg').opt_prefix}/lib"
         s.change_make_var! "JPG_LIB"  , "-ljpeg"
 
-        # add link-flags
-        s.change_make_var! "LINKFLAGS"  , ENV["LDFLAGS"]
-        s.change_make_var! "SHLIBFLAGS" , "-shared #{ENV['LDFLAGS']}"
+        s.change_make_var! "CCFLAGS", ENV["CFLAGS"]
+        s.change_make_var! "LIB", ENV["LDFLAGS"]
+      end
+
+      inreplace "VORONOI/Makefile.lammps" do |s|
+        s.change_make_var! "voronoi_SYSINC", "-I#{HOMEBREW_PREFIX}/include/voro++"
       end
 
       # setup standard packages
@@ -227,7 +256,11 @@ class Lammps < Formula
   # without this the module cannot find the library when homebrew is installed in a
   # custom directory.
   def patches
-    DATA
+    p = [ DATA,]
+    # user-omp has a bug because it assumes gnu
+    # patch submitted upstream, USER-OMP is currently being rewritten as a standard package and this patch may soon become obsolete
+    p << "https://gist.github.com/scicalculator/4759616/raw/3b9b1ad9b38f0f20a52e63e8c7add9780056b2ca/user-omp_bsd.patch" if build.include? "enable-user-omp"
+    p
   end
 end
 
